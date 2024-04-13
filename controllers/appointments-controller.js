77d99bdaf69a444625b72bc0b05d9711d69717fd
ReyObejero <rey_obejero@dlsu.edu.ph>
2024-04-13 16:25:30 +0800

    docs: Document the CRUD operations in /controllers/appointments-controller.js

--- controllers/appointments-controller.js
+++ controllers/appointments-controller.js
@@ -4,6 +4,18 @@ import {
     POOL_VISMIN,
 } from '../database.js';
 
+/**
+ * Inserts a new appointment record in the 'denormalizedappointments' table
+ * using the Serializable isolation level
+ * @param {String} req.body.clinicId Clinic ID of the appointment
+ * @param {String} req.body.queueDate Queue date of the appointment
+ * @param {String} req.body.startTime Start time of the appointment
+ * @param {String} req.body.type Type of the appointment
+ * @param {String} req.body.virtual Virtual status of the appointment
+ * @param {Object} res Response object to send HTTP response back to the client
+ * @returns {Promise<Object>} A promise that resolves with an object detailing
+ *     the result of the insert request
+ */
 const createAppointment = async (req, res) => {
     try {
         const {
@@ -31,7 +43,11 @@ const createAppointment = async (req, res) => {
         await POOL_PHILIPPINES.query(`START TRANSACTION;`);
 
         const RESULT = await POOL_PHILIPPINES.query(QUERY, [
-            clinicId, queueDate, startTime, type, virtual,
+            clinicId,
+            queueDate,
+            startTime,
+            type,
+            virtual,
         ]);
 
         await POOL_PHILIPPINES.query(`COMMIT;`);
@@ -42,6 +58,14 @@ const createAppointment = async (req, res) => {
     }
 };
 
+/**
+ * Deletes an appointment record in the 'denormalizedappointments' table
+ * using the Serializable isolation level
+ * @param {String} req.body.appointmentId Appointment ID of the appointment
+ * @param {Object} res Response object to send HTTP response back to the client
+ * @returns {Promise<Object>} A promise that resolves with an object detailing
+ *     the result of the delete request
+ */
 const deleteAppointment = async (req, res) => {
     try {
         const APPOINTMENT_ID = req.body.appointmentId;
@@ -63,17 +87,25 @@ const deleteAppointment = async (req, res) => {
     }
 };
 
+/**
+ * Reads an appointment record from the 'denormalizedappointments' table
+ * using the Read Uncommitted isolation level
+ * @param {String} req.params.appointmentId Appointment ID of the appointment
+ * @param {Object} res Response object to send HTTP response back to the client
+ * @returns {Promise<Array<Object>>} Promise that resolves with an array of
+ *     objects whose appointmentId matches the appointmentId argument
+ */
 const getAppointment = async (req, res) => {
     try {
         const APPOINTMENT_ID = req.params.appointmentId;
         const QUERY = 'SELECT * FROM denormalizedappointments WHERE apptid = ?;';
 
-        await POOL_LUZON.query(`
+        await POOL_PHILIPPINES.query(`
             SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
         `);
-        await POOL_LUZON.query(`START TRANSACTION;`);
+        await POOL_PHILIPPINES.query(`START TRANSACTION;`);
 
-        const [ROWS] = await POOL_LUZON.query(QUERY, APPOINTMENT_ID);
+        const ROWS = await POOL_PHILIPPINES.query(QUERY, APPOINTMENT_ID);
 
         res.status(200).json(ROWS);
     } catch (error) {
@@ -91,7 +123,7 @@ const getAppointments = async (req, res) => {
         `);
         await POOL_PHILIPPINES.query(`START TRANSACTION;`);
 
-        const [ROWS] = await POOL_PHILIPPINES.query(QUERY);
+        const ROWS = await POOL_PHILIPPINES.query(QUERY);
 
         res.status(200).json(ROWS);
     } catch (error) {
@@ -112,6 +144,20 @@ const getUpdateAppointments = async (req, res) => {
     res.render('update-appointments', {title: 'Update Appointment'});
 };
 
+/**
+ * Updates an appointment record in the 'denormalizedappointments' table
+ * using the Serializable isolation level
+ * @param {String} req.body.appointmentId Appointment ID of the appointment
+ * @param {String} req.body.endTime New End Time of the appointment
+ * @param {String} req.body.queueDate New Queue Date of the appointment
+ * @param {String} req.body.startTime New Start Time of the appointment
+ * @param {String} req.body.status New status of the appointment
+ * @param {String} req.body.type New type of the appointment
+ * @param {String} req.body.virtual New virtual status of the appointment
+ * @param {Object} res Response object to send HTTP response back to the client
+ * @returns {Promise<Object>} Promise that resolves with an object detailing
+ *     the result of the update request
+ */
 const updateAppointment = async (req, res) => {
     try {
         const {
